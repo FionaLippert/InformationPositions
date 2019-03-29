c2471cf399ff659112b8ceb6634067a9f2beb78d
Fiona Lippert <fiona@lipperta.de>
2019-03-29 11:44:03 +0100

    network nodes

---
+++ get_nodes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# __author__ = 'Fiona Lippert'
+
+import networkx as nx
+import numpy as np
+import os
+import glob
+
+for filepath in glob.iglob('networkData/*.gpickle'):
+    G = nx.read_gpickle(filepath)
+    nodes = list(G)
+    path = filepath.strip('.gpickle')
+    np.save(path + '_nodes.npy', np.array(nodes))
