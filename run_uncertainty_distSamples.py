2a14e4bcaaea885b380861c3294004c110c61b0f
Fiona Lippert <fiona@lipperta.de>
2019-03-30 17:35:08 +0100

    find Tc

--- run_uncertainty_distSamples.py
+++ run_uncertainty_distSamples.py
@@ -19,6 +19,33 @@ nthreads = mp.cpu_count() - 1 # leave one thread for coordination tasks
 #nthreads = 1
 
 
+def computeMI_joint(jointSnapshots, maxDist, Z):
+    MIs = []
+    for d in range(maxDist):
+        P_XY = np.array(sorted([v/Z for s in jointSnapshots[d].keys() for v in jointSnapshots[d][s].values()]))
+        P_X = np.array([sum(list(dict_s.values()))/Z for dict_s in jointSnapshots[d].values()])
+        all_keys = set.union(*[set(dict_s.keys()) for dict_s in jointSnapshots[d].values()])
+        P_Y = np.array([jointSnapshots[d][1][k]/Z if k in jointSnapshots[d][1] else 0 for k in all_keys]) + \
+                np.array([jointSnapshots[d][-1][k]/Z if k in jointSnapshots[d][-1] else 0 for k in all_keys])
+
+        #print(P_XY, P_Y, P_X)
+
+        MI = stats.entropy(P_X, base=2) + stats.entropy(P_Y, base=2) - stats.entropy(P_XY, base=2)
+        MIs.append(MI)
+    return MIs
+
+def computeMI_joint_array(jointSnapshots, maxDist, Z):
+    MIs = []
+    for d in range(maxDist):
+        P_XY = jointSnapshots[d].flatten()/Z
+        P_X = np.sum(jointSnapshots[d], axis=1)/Z # sum over all bins
+        P_Y = np.sum(jointSnapshots[d], axis=0)/Z # sum over all spin states
+
+        MI = stats.entropy(P_X, base=2) + stats.entropy(P_Y, base=2) - stats.entropy(P_XY, base=2)
+        MIs.append(MI)
+    return MIs
+
+
 
 def computeMI_cond(model, node, dist, neighbours_G, snapshots, nTrials, nSamples, modelSettings, corrTimeSettings, threshold):
     MIs = []
@@ -51,7 +78,7 @@ def computeMI_cond(model, node, dist, neighbours_G, snapshots, nTrials, nSamples
 
 if __name__ == '__main__':
 
-    start = timer()
+
 
     # create data directory
     now = time.time()
@@ -92,7 +119,7 @@ if __name__ == '__main__':
 
     # setup Ising model with nNodes spin flip attempts per simulation step
     modelSettings = dict( \
-        temperature     = 1.0, \
+        temperature     = 0.75, \
         updateType      = 'async' ,\
         magSide         = ''
     )
@@ -104,78 +131,128 @@ if __name__ == '__main__':
     allNeighbours_G, allNeighbours_idx = model.neighboursAtDist(model.mapping[node], maxDist)
 
 
-    # determine mixing/correlation time
-    mixingTimeSettings = dict( \
-        nInitialConfigs = 10, \
-        burninSteps  = 10, \
-        nStepsRegress   = int(1e3), \
-        nStepsCorr      = int(1e4), \
-        thresholdReg    = 0.1, \
-        thresholdCorr   = 0.01
-    )
-    IO.saveSettings(targetDirectory, mixingTimeSettings, 'mixingTime')
-    mixingTime, meanMag, distSamples, mags = infcy.determineCorrTime(model, **mixingTimeSettings)
-    print(f'correlation time = {distSamples}')
-    print(f'mixing time      = {mixingTime}')
-    print(f'mean mags        = {meanMag}')
-
-    for key, values in mags.items():
-        np.save(f'{targetDirectory}/magSeries_{key}.npy', np.array(values))
+    if sys.argv[1] == 'cond':
 
 
-    corrTimeSettings = dict( \
-        nInitialConfigs = 10, \
-        burninSteps  = mixingTime, \
-        nStepsCorr      = int(1e4), \
-        checkMixing     = 0, \
-        node            = node
-    )
-    IO.saveSettings(targetDirectory, corrTimeSettings, 'corrTime')
+        # determine mixing/correlation time
+        mixingTimeSettings = dict( \
+            nInitialConfigs = 10, \
+            burninSteps  = 10, \
+            nStepsRegress   = int(1e3), \
+            nStepsCorr      = int(1e4), \
+            thresholdReg    = 0.1, \
+            thresholdCorr   = 0.01
+        )
+        IO.saveSettings(targetDirectory, mixingTimeSettings, 'mixingTime')
+        mixingTime, meanMag, distSamples, mags = infcy.determineCorrTime(model, **mixingTimeSettings)
+        print(f'correlation time = {distSamples}')
+        print(f'mixing time      = {mixingTime}')
+        print(f'mean mags        = {meanMag}')
+
+        #for key, values in mags.items():
+        #    np.save(f'{targetDirectory}/magSeries_{key}.npy', np.array(values))
 
 
-    # collect neighbourhood snapshots
-    nSnapshots = 50
-    snapshotSettingsCond = dict( \
-        nSamples    = nSnapshots, \
-        burninSamples = mixingTime, \
-        maxDist     = maxDist
-    )
-    IO.saveSettings(targetDirectory, snapshotSettingsCond, 'snapshots')
+        corrTimeSettings = dict( \
+            nInitialConfigs = 10, \
+            burninSteps  = mixingTime, \
+            nStepsCorr      = int(1e4), \
+            checkMixing     = 0, \
+            node            = node
+        )
+        IO.saveSettings(targetDirectory, corrTimeSettings, 'corrTime')
 
 
+        # collect neighbourhood snapshots
+        nSnapshots = 50
+        snapshotSettingsCond = dict( \
+            nSamples    = nSnapshots, \
+            burninSamples = mixingTime, \
+            maxDist     = maxDist
+        )
+        IO.saveSettings(targetDirectory, snapshotSettingsCond, 'snapshots')
 
 
-    snapshots, _ = infcy.getSnapshotsPerDist2(model, node, allNeighbours_idx, **snapshotSettingsCond, threads=nthreads)
+        snapshots, _ = infcy.getSnapshotsPerDist2(model, node, allNeighbours_idx, **snapshotSettingsCond, threads=nthreads)
 
-    #state = list(snapshots[maxDist-1].keys())[0]
-    #mixingTime, meanMag, distSamples, _ = infcy.determineCorrTime(model, **corrTimeSettings)
-    #print(f'correlation time = {distSamples}')
-    #print(f'mixing time      = {mixingTime}')
 
-    nTrials = 1 # 10
-    nSamples = 1000
-    reps = 10
+        nTrials = 1 # 10
+        nSamples = 1000
+        reps = 10
 
-    thresholds = np.linspace(0.9, 0.01, 10)
-    np.save(f'{targetDirectory}/corrThresholds.npy', np.array(thresholds))
+        thresholds = np.linspace(0.9, 0.01, 10)
+        np.save(f'{targetDirectory}/corrThresholds.npy', np.array(thresholds))
 
-    #for dist in [1, 2, 3, 4]:
-    for dist in [5,6]:
+        #for dist in [1, 2, 3, 4]:
+        for dist in [1, 2, 3, 4, 5,6]:
 
-        numStates = len(snapshots[dist-1])
+            numStates = len(snapshots[dist-1])
 
-        #distances = np.logspace(0,3,13).astype(int)
+            #distances = np.logspace(0,3,13).astype(int)
 
-        p = np.zeros((thresholds.size, reps, numStates))
+            p = np.zeros((thresholds.size, reps, numStates))
+
+            for i, t in enumerate(thresholds):
+                print(f'------------- threshold = {t} -------------')
+
+                for rep in range(reps):
+                    #snapshots, _ = infcy.getSnapshotsPerDist2(model, node, allNeighbours_idx, **snapshotSettingsCond, threads=nthreads)
+                    p[i, rep,:] = computeMI_cond(model, node, dist, allNeighbours_G, snapshots, nTrials, nSamples, modelSettings, corrTimeSettings, t)[:,0]
+                np.save(f'{targetDirectory}/p_cond_T={model.t}_d={dist}.npy', np.array(p))
+
+    elif sys.argv[1] == 'avg':
+
+        thresholds = np.linspace(0.9, 0.01, 10)
+        thresholds = np.array([0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.01])
+        np.save(f'{targetDirectory}/corrThresholds.npy', np.array(thresholds))
+
+        nBins = 100
+        reps = 10
+
+        MI = np.zeros((thresholds.size, reps, maxDist))
 
         for i, t in enumerate(thresholds):
             print(f'------------- threshold = {t} -------------')
 
-            for rep in range(reps):
-                #snapshots, _ = infcy.getSnapshotsPerDist2(model, node, allNeighbours_idx, **snapshotSettingsCond, threads=nthreads)
-                p[i, rep,:] = computeMI_cond(model, node, dist, allNeighbours_G, snapshots, nTrials, nSamples, modelSettings, corrTimeSettings, t)[:,0]
-            np.save(f'{targetDirectory}/p_cond_T={model.t}_d={dist}.npy', np.array(p))
+            # determine mixing/correlation time
+            mixingTimeSettings = dict( \
+                nInitialConfigs = 10, \
+                burninSteps  = 10, \
+                nStepsRegress   = int(1e3), \
+                nStepsCorr      = int(1e4), \
+                thresholdReg    = 0.1, \
+                thresholdCorr   = t
+            )
+            IO.saveSettings(targetDirectory, mixingTimeSettings, 'mixingTime')
+            mixingTime, meanMag, distSamples, mags = infcy.determineCorrTime(model, **mixingTimeSettings)
+            print(f'correlation time = {distSamples}')
+            print(f'mixing time      = {mixingTime}')
+            print(f'mean mags        = {meanMag}')
 
-    print(f'time elapsed: {timer()-start : .2f} seconds')
+            snapshotSettingsJoint = dict( \
+                nSamples    = int(1e4), \
+                repeats     = 10, \
+                burninSamples = mixingTime, \
+                distSamples   = distSamples, \
+                maxDist     = maxDist, \
+                nBins       = nBins
+            )
+            IO.saveSettings(targetDirectory, snapshotSettingsJoint, 'jointSnapshots')
+
+            #for key, values in mags.items():
+            #    np.save(f'{targetDirectory}/magSeries_{key}.npy', np.array(values))
+
+            start = timer()
+            for rep in range(reps):
+                print(f'rep {rep}')
+                start_rep = timer()
+                _, avgSnapshots = infcy.getJointSnapshotsPerDist2(model, node, allNeighbours_idx, **snapshotSettingsJoint, threads = nthreads)
+                Z = snapshotSettingsJoint['nSamples'] * snapshotSettingsJoint['repeats']
+                MI[i, rep, :] = computeMI_joint_array(avgSnapshots, maxDist, Z)
+                print(f'time for one run: {timer()-start_rep : .2f} seconds')
+            print(f'time elapsed: {timer()-start : .2f} seconds')
+            np.save(f'{targetDirectory}/MI_cond_T={model.t}.npy', np.array(MI))
+
+
 
     print(targetDirectory)
